b4b5a56789a7b69413515b9d423f0429f68005b5
FionaMacRaeFairlie <fionamacraefairlie@gmail.com>
2022-02-15 17:09:55 +0000

    updated to use express urlencoded

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: false }));
 
-/*app.use(express.urlencoded({
-    extended: true
-  }))
-  const bodyParser = require('body-parser');
-  //app.use(bodyParser.urlencoded({extended: false}));
-  */
-  const bodyParser = require('body-parser');
-  app.use(bodyParser.urlencoded({
-      extended: false
-  }));
-  
+const path = require('path');
+const public = path.join(__dirname,'public');
+app.use(express.static(public));
 
-  const path = require('path');
-  const public = path.join(__dirname,'public');
-  app.use(express.static(public));
-
-//app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-//app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 const mustache = require('mustache-express');
